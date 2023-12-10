1da12b86a7be5a2a73568c18c6d7d7c6c021759c
lpoulter1 <thundercatontheloose@gmail.com>
2023-12-10 16:44:15 +0000

    add some pink cards and start of auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,30 @@ import activitiesJson from "./sample-data/activities.json";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <h1 className="text-2xl text-fuchsia-500">Run a Mile</h1>
-      <div>
-        <ActivityTable activities={activitiesJson} />
+    <main className="min-h-screen bg-slate-800 text-white">
+      <h1 className="text-2xl ">Run a Mile</h1>
+      <a
+        className="text-lime-300"
+        href="http://www.strava.com/oauth/authorize?client_id=117837&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read,activity:read_all"
+      >
+        Load from Strava
+      </a>
+      <div>{activitiesJson.length} activities</div>
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(320px,_1fr))] gap-8">
+        {activitiesJson.map((activity: Activity) => (
+          <ActivityCard key={activity.id} activity={activity} />
+        ))}
       </div>
     </main>
+  );
+}
+
+function ActivityCard({ activity }: { activity: Activity }) {
+  return (
+    <div className="flex bg-fuchsia-300 w-80 flex-col rounded-sm h-40 justify-center items-center">
+      <h2>Date {activity.start_date}</h2>
+      <div>Distance{activity.distance}</div>
+    </div>
   );
 }
 
